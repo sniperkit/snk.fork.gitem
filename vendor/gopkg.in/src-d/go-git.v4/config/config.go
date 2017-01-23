b82f97d6330a0e5d1cf8ae5bdd7d663fdae9d933
stxmendez <stxmendez@gmail.com>
2017-01-22 23:18:32 -0500

    Reworked the use of glide. I inavertantly added some of the vendored deps as if they were git submodules.  Not entirely clear how I did that, but it appears to be sorted out now.

---
+++ vendor/gopkg.in/src-d/go-git.v4/config/config.go
@@ -0,0 +1,81 @@
+// Package config storage is the implementation of git config for go-git
+package config
+
+import (
+	"errors"
+	"fmt"
+)
+
+const (
+	// DefaultFetchRefSpec is the default refspec used for fetch.
+	DefaultFetchRefSpec = "+refs/heads/*:refs/remotes/%s/*"
+	// DefaultPushRefSpec is the default refspec used for push.
+	DefaultPushRefSpec = "refs/heads/*:refs/heads/*"
+)
+
+// ConfigStorer generic storage of Config object
+type ConfigStorer interface {
+	Config() (*Config, error)
+	SetConfig(*Config) error
+}
+
+var (
+	ErrInvalid               = errors.New("config invalid remote")
+	ErrRemoteConfigNotFound  = errors.New("remote config not found")
+	ErrRemoteConfigEmptyURL  = errors.New("remote config: empty URL")
+	ErrRemoteConfigEmptyName = errors.New("remote config: empty name")
+)
+
+// Config contains the repository configuration
+type Config struct {
+	Core struct {
+		IsBare bool
+	}
+	Remotes map[string]*RemoteConfig
+}
+
+// NewConfig returns a new empty Config
+func NewConfig() *Config {
+	return &Config{
+		Remotes: make(map[string]*RemoteConfig, 0),
+	}
+}
+
+// Validate validate the fields and set the default values
+func (c *Config) Validate() error {
+	for name, r := range c.Remotes {
+		if r.Name != name {
+			return ErrInvalid
+		}
+
+		if err := r.Validate(); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// RemoteConfig contains the configuration for a given repository
+type RemoteConfig struct {
+	Name  string
+	URL   string
+	Fetch []RefSpec
+}
+
+// Validate validate the fields and set the default values
+func (c *RemoteConfig) Validate() error {
+	if c.Name == "" {
+		return ErrRemoteConfigEmptyName
+	}
+
+	if c.URL == "" {
+		return ErrRemoteConfigEmptyURL
+	}
+
+	if len(c.Fetch) == 0 {
+		c.Fetch = []RefSpec{RefSpec(fmt.Sprintf(DefaultFetchRefSpec, c.Name))}
+	}
+
+	return nil
+}
